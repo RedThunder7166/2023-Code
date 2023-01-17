35a9ec649c6a8fb40427754a854406ccbbac376e
Red Thunder Robotics <LHSRobotics7166@gmail.com>
2023-01-17 17:59:01 -0500

    added getPressedName method to Buttons

--- 2023_Code/src/main/java/frc/robot/subsystems/Buttons.java
+++ 2023_Code/src/main/java/frc/robot/subsystems/Buttons.java
@@ -14,48 +14,67 @@ public class Buttons extends SubsystemBase {
   /** Creates a new Buttons. */
   GenericHID m_buttonBoard = new GenericHID(0);
 
-  // String[] buttons = {
-  //   "nodeone",
-  //   "nodetwo",
-  //   "nodethree",
-  //   "nodefour",
-  //   "nodefive",
-  //   "nodesix",
-  //   "nodeseven",
-  //   "nodeeight",
-  //   "nodenine",
-  //   "reset",
-  //   "stop",
-  //   "wristup",
-  //   "wristdown"
-  // };
-  int kNodeOneButton = 1;
-  int kNodeTwoButton = 2;
-  int kNodeThreeButton = 3;
-  int kNodeFourButton = 4;
-  int kNodeFiveButton = 5;
-  int kNodeSixButton = 6;
-  int kNodeSevenButton = 7;
-  int kNodeEightButton = 8;
-  int kNodeNineButton = 9;
+  String[] buttons = {
+    "nodeone",
+    "nodetwo",
+    "nodethree",
+    "nodefour",
+    "nodefive",
+    "nodesix",
+    "nodeseven",
+    "nodeeight",
+    "nodenine",
+    "reset",
+    "stop",
+    "wristup",
+    "wristdown"
+  };
+  // int kNodeOneButton = 1;
+  // int kNodeTwoButton = 2;
+  // int kNodeThreeButton = 3;
+  // int kNodeFourButton = 4;
+  // int kNodeFiveButton = 5;
+  // int kNodeSixButton = 6;
+  // int kNodeSevenButton = 7;
+  // int kNodeEightButton = 8;
+  // int kNodeNineButton = 9;
   
-  int kResetButton = 10;
-  int kStopButton = 11;
-  int kWristUpButton = 12;
-  int kWristDownButton = 13;
+  // int kResetButton = 10;
+  // int kStopButton = 11;
+  // int kWristUpButton = 12;
+  // int kWristDownButton = 13;
 
   public Buttons() {
   }
   public boolean isPressed(int id) {
     return m_buttonBoard.getRawButton(id);
   }
+
+  /**
+   * Gets the ID of the button being pressed with the lowest ID.
+   * @return <int> button ID
+   */
   public int getPressedId() {
-    for (int i = 1; i < 14; i++) {
-      if (isPressed(i)) {
-        return i;
+    // for (int i = 1; i < 14; i++) {
+    //   if (isPressed(i)) {
+    //     return i;
+    //   }
+    // }
+    // return 0;
+    for (int id = 0; id < buttons.length; id++) {
+      // we add one to id because arrays start at 0 while the button board starts at 1
+      if (isPressed(id + 1)) {
+        return id + 1;
       }
     }
     return 0;
+  }
+  public String getPressedName(){
+    int index = getPressedId() - 1;
+    if (index < 0) {
+      return "none";
+    }
+    return buttons[index];
   }
   
   @Override
@@ -115,14 +134,3 @@ public class Buttons extends SubsystemBase {
     // }
   }
 }
-
-
-
-
-
-
-
-
-
-
-
