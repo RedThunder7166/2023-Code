978d3eaa0af30e853688a506c5a2986fe47c2990
Paul Garrett <LHSRobotics7166@gmail.com>
2023-01-22 18:56:30 -0500

    Pose Estimator Attempt

--- 2023_Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ 2023_Code/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -6,11 +6,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.GenericEntry;
 
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+
 import com.ctre.phoenix.sensors.WPI_Pigeon2;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -33,91 +38,89 @@ public class DriveSubsystem extends SubsystemBase {
   private ShuffleboardTab swerveTab = Shuffleboard.getTab("SDS Swerve");
   private ShuffleboardTab moduleTab = Shuffleboard.getTab("Module Info");
 
-  private GenericEntry xSpeedEntry = 
-  swerveTab.add("xBox xSpeed", 0)
-          .getEntry();
+  private VisionSubsystem m_VisionSubsystem;
+  private Optional<EstimatedRobotPose> m_EstimatedRobotPose;
+  private EstimatedRobotPose m_robotPose;
 
-  private GenericEntry ySpeedEntry = 
-  swerveTab.add("xBox ySpeed", 0)
-          .getEntry();
+  private GenericEntry xSpeedEntry = swerveTab.add("xBox xSpeed", 0)
+      .getEntry();
 
-  private GenericEntry rotEntry = 
-  swerveTab.add("xBox rot", 0)
-          .getEntry();
+  private GenericEntry ySpeedEntry = swerveTab.add("xBox ySpeed", 0)
+      .getEntry();
 
-  private GenericEntry frontLeftStateEntry =
-  swerveTab.add("FL State v", 0)
-          .getEntry();
-  
-  private GenericEntry frontRightStateEntry =
-  swerveTab.add("FR State v", 0)
-          .getEntry();
+  private GenericEntry rotEntry = swerveTab.add("xBox rot", 0)
+      .getEntry();
 
-  private GenericEntry gyroEntry =
-  swerveTab.add("Gyro Heading", 0)
-          .getEntry();
+  private GenericEntry frontLeftStateEntry = swerveTab.add("FL State v", 0)
+      .getEntry();
 
+  private GenericEntry frontRightStateEntry = swerveTab.add("FR State v", 0)
+      .getEntry();
 
-  private final SwerveModule m_frontLeft = 
-    new SwerveModule(
+  private GenericEntry gyroEntry = swerveTab.add("Gyro Heading", 0)
+      .getEntry();
+
+  private final SwerveModule m_frontLeft = new SwerveModule(
       DriveConstants.kFrontLeftDriveMotorPort,
       DriveConstants.kFrontLeftTurningMotorPort,
       DriveConstants.kFrontLeftTurningEncoderPorts,
       DriveConstants.kFrontLeftAngleZero,
       moduleTab.getLayout("Front Left Module", BuiltInLayouts.kList)
-      .withSize(2, 4)
-      .withPosition(0, 0));
+          .withSize(2, 4)
+          .withPosition(0, 0));
 
-  private final SwerveModule m_rearLeft =
-    new SwerveModule(
-      DriveConstants.kRearLeftDriveMotorPort, 
-      DriveConstants.kRearLeftTurningMotorPort, 
+  private final SwerveModule m_rearLeft = new SwerveModule(
+      DriveConstants.kRearLeftDriveMotorPort,
+      DriveConstants.kRearLeftTurningMotorPort,
       DriveConstants.kRearLeftTurningEncoderPorts,
       DriveConstants.kRearLeftAngleZero,
       moduleTab.getLayout("Rear Left Module", BuiltInLayouts.kList)
-      .withSize(2, 4)
-      .withPosition(2, 0));
+          .withSize(2, 4)
+          .withPosition(2, 0));
 
-  private final SwerveModule m_frontRight = 
-    new SwerveModule(
-      DriveConstants.kFrontRightDriveMotorPort, 
-      DriveConstants.kFrontRightTurningMotorPort, 
+  private final SwerveModule m_frontRight = new SwerveModule(
+      DriveConstants.kFrontRightDriveMotorPort,
+      DriveConstants.kFrontRightTurningMotorPort,
       DriveConstants.kFrontRightTurningEncoderPorts,
       DriveConstants.kFrontRightAngleZero,
       moduleTab.getLayout("Front Right Module", BuiltInLayouts.kList)
-      .withSize(2, 4)
-      .withPosition(4, 0));
+          .withSize(2, 4)
+          .withPosition(4, 0));
 
-  private final SwerveModule m_rearRight = 
-    new SwerveModule(
-      DriveConstants.kRearRightDriveMotorPort, 
-      DriveConstants.kRearRightTurningMotorPort, 
+  private final SwerveModule m_rearRight = new SwerveModule(
+      DriveConstants.kRearRightDriveMotorPort,
+      DriveConstants.kRearRightTurningMotorPort,
       DriveConstants.kRearRightTurningEncoderPorts,
       DriveConstants.kRearRightAngleZero,
       moduleTab.getLayout("Rear Right Module", BuiltInLayouts.kList)
-      .withSize(2, 4)
-      .withPosition(6, 0));
-
- 
+          .withSize(2, 4)
+          .withPosition(6, 0));
 
   // Initializing the gyro sensor
-  //private final Gyro m_gyro = new ADXRS450_Gyro();
+  // private final Gyro m_gyro = new ADXRS450_Gyro();
   private final WPI_Pigeon2 m_gyro = new WPI_Pigeon2(26);
 
-  // Odometry class for tracking robot pose
-  SwerveDriveOdometry m_odometry =
-    new SwerveDriveOdometry(DriveConstants.kDriveKinematics, m_gyro.getRotation2d(), 
+  // Odometry class for tracking robot pose, now with vision!
+
+
+  SwerveDrivePoseEstimator m_odometry = new SwerveDrivePoseEstimator(DriveConstants.kDriveKinematics,
+      m_gyro.getRotation2d(),
       new SwerveModulePosition[] {
-        new SwerveModulePosition(m_frontLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
-        new SwerveModulePosition(m_frontRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
-        new SwerveModulePosition(m_rearLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
-        new SwerveModulePosition(m_rearRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
-      }
-    );
+          new SwerveModulePosition(m_frontLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
+          new SwerveModulePosition(m_frontRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
+          new SwerveModulePosition(m_rearLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
+          new SwerveModulePosition(m_rearRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
+      },
+      new Pose2d(0, 0, new Rotation2d(0)) // TODO: May need to tune this pose2d
+  );
 
-    /** Creates a new DriveSubsystem. */
-  public DriveSubsystem() {}
+  /** Creates a new DriveSubsystem. */
+  public DriveSubsystem() {
+  }
 
+  public void addVisionSubsystem(VisionSubsystem m_VisionSubsystem) {
+    this.m_VisionSubsystem = m_VisionSubsystem;
+  }
 
   @Override
   public void periodic() {
@@ -128,57 +131,69 @@ public class DriveSubsystem extends SubsystemBase {
     SmartDashboard.putNumber("Gyro Entry 2023", m_gyro.getAngle());
     SmartDashboard.putNumber("Pitch", m_gyro.getPitch());
 
-
     m_odometry.update(
-      m_gyro.getRotation2d(),
-      new SwerveModulePosition[] {
-        new SwerveModulePosition(m_frontLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
-        new SwerveModulePosition(m_frontRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
-        new SwerveModulePosition(m_rearLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
-        new SwerveModulePosition(m_rearRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
-      }
-      );
+        m_gyro.getRotation2d(),
+        new SwerveModulePosition[] {
+            new SwerveModulePosition(m_frontLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
+            new SwerveModulePosition(m_frontRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
+            new SwerveModulePosition(m_rearLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
+            new SwerveModulePosition(m_rearRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
+        });
+
+    m_EstimatedRobotPose = m_VisionSubsystem.getEstimatedGlobalPose(getPose());
+    if (m_EstimatedRobotPose.isPresent()) {
+      m_robotPose = m_EstimatedRobotPose.get();
+      m_odometry.addVisionMeasurement(m_EstimatedRobotPose.get().estimatedPose.toPose2d(),
+          m_EstimatedRobotPose.get().timestampSeconds);
+
+    }
+    SmartDashboard.putNumber("X", m_odometry.getEstimatedPosition().getX());
+    SmartDashboard.putNumber("Y", m_odometry.getEstimatedPosition().getY());
+    SmartDashboard.putNumber("Angle", m_odometry.getEstimatedPosition().getRotation().getDegrees());
+
   }
 
   // Returns the currently-estimated pose of the robot
 
-  public Pose2d getPose(){
-    return m_odometry.getPoseMeters();
+  public Pose2d getPose() {
+    return m_odometry.getEstimatedPosition();
   }
 
   // Resets the odometry to the specified pose
 
-  public void resetOdometry(Pose2d pose){
-    m_odometry.resetPosition(m_gyro.getRotation2d(), 
-    new SwerveModulePosition[] {
-      new SwerveModulePosition(m_frontLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
-      new SwerveModulePosition(m_frontRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
-      new SwerveModulePosition(m_rearLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
-      new SwerveModulePosition(m_rearRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
-    }, pose); // TODO: CONFIRM THAT WE DIDNT SCREW UP
+  public void resetOdometry(Pose2d pose) {
+    m_odometry.resetPosition(m_gyro.getRotation2d(),
+        new SwerveModulePosition[] {
+            new SwerveModulePosition(m_frontLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
+            new SwerveModulePosition(m_frontRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
+            new SwerveModulePosition(m_rearLeft.getPosition(), new Rotation2d(m_gyro.getAngle())),
+            new SwerveModulePosition(m_rearRight.getPosition(), new Rotation2d(m_gyro.getAngle())),
+        }, pose); // TODO: CONFIRM THAT WE DIDNT SCREW UP
   }
 
-  /**  Method to drive the robot using joystick info
-   * @param xSpeed Speed of the robot in the x direction (forward).
-   * @param ySpeed Speed of the robot in the y direction (sideways).
-   * @param rot Angular rate of the robot.
-   * @param fieldRelative Whether the provided x and y speeds are relative to the field.
+  /**
+   * Method to drive the robot using joystick info
+   * 
+   * @param xSpeed        Speed of the robot in the x direction (forward).
+   * @param ySpeed        Speed of the robot in the y direction (sideways).
+   * @param rot           Angular rate of the robot.
+   * @param fieldRelative Whether the provided x and y speeds are relative to the
+   *                      field.
    */
   @SuppressWarnings("ParameterName")
-  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative){
-    
-    var swerveModuleStates = 
-      DriveConstants.kDriveKinematics.toSwerveModuleStates(
+  public void drive(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
+
+    var swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
         fieldRelative
-          ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
-          : new ChassisSpeeds(xSpeed, ySpeed, rot));
+            ? ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, m_gyro.getRotation2d())
+            : new ChassisSpeeds(xSpeed, ySpeed, rot));
     SwerveDriveKinematics.desaturateWheelSpeeds(
-      swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
+        swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond);
 
     m_frontLeft.setDesiredState(swerveModuleStates[0]);
     m_frontRight.setDesiredState(swerveModuleStates[1]);
     m_rearLeft.setDesiredState(swerveModuleStates[2]);
-    m_rearRight.setDesiredState(swerveModuleStates[3]); 
+    m_rearRight.setDesiredState(swerveModuleStates[3]);
 
     SmartDashboard.putNumber("FL Mod Heading", m_frontLeft.getModuleHeading());
     SmartDashboard.putNumber("FR Mod Heading", m_frontRight.getModuleHeading());
@@ -194,7 +209,7 @@ public class DriveSubsystem extends SubsystemBase {
 
   }
 
-    /**
+  /**
    * Sets the swerve ModuleStates.
    *
    * @param desiredStates The desired SwerveModule states.
@@ -202,15 +217,15 @@ public class DriveSubsystem extends SubsystemBase {
 
   public void setModuleStates(SwerveModuleState[] desiredStates) {
     SwerveDriveKinematics.desaturateWheelSpeeds(
-      desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
-      m_frontLeft.setDesiredState(desiredStates[0]);
-      m_frontRight.setDesiredState(desiredStates[1]);
-      m_rearLeft.setDesiredState(desiredStates[2]);
-      m_rearRight.setDesiredState(desiredStates[3]);
+        desiredStates, DriveConstants.kMaxSpeedMetersPerSecond);
+    m_frontLeft.setDesiredState(desiredStates[0]);
+    m_frontRight.setDesiredState(desiredStates[1]);
+    m_rearLeft.setDesiredState(desiredStates[2]);
+    m_rearRight.setDesiredState(desiredStates[3]);
   }
 
   // Resets the drive encoders to currently read a position of 0
-  public void resetEncoders(){
+  public void resetEncoders() {
     m_frontLeft.resetEncoders();
     m_rearLeft.resetEncoders();
     m_frontRight.resetEncoders();
@@ -222,43 +237,41 @@ public class DriveSubsystem extends SubsystemBase {
     m_gyro.reset();
   }
 
-    /**
+  /**
    * Returns the heading of the robot.
    *
    * @return the robot's heading in degrees, from -180 to 180
    */
-  public double getHeading(){
+  public double getHeading() {
     return m_gyro.getRotation2d().getDegrees();
   }
 
-    /**
+  /**
    * Returns the turn rate of the robot.
    *
    * @return The turn rate of the robot, in degrees per second
    */
-  public double getTurnRate(){
+  public double getTurnRate() {
     return m_gyro.getRate() * (DriveConstants.kGyroReversed ? -1.0 : 1.0);
   }
 
-  public Command followTrajectoryCommand(PathPlannerTrajectory traj, boolean isFirstPath){
+  public Command followTrajectoryCommand(PathPlannerTrajectory traj, boolean isFirstPath) {
     return new SequentialCommandGroup(
-      new InstantCommand(() -> {
-        //Reset odometry for the firs thatt you run during auto
-        if(isFirstPath){
-          this.resetOdometry(traj.getInitialHolonomicPose());
-        }
-      }),
-      new PPSwerveControllerCommand(
-        traj,
-        this::getPose, //Pose supplier
-        DriveConstants.kDriveKinematics,
-        new PIDController(0, 0, 0),
-        new PIDController(0, 0, 0),
-        new PIDController(0, 0, 0),
-        this::setModuleStates,
-        true,
-        this
-      )
-    );
+        new InstantCommand(() -> {
+          // Reset odometry for the firs thatt you run during auto
+          if (isFirstPath) {
+            this.resetOdometry(traj.getInitialHolonomicPose());
+          }
+        }),
+        new PPSwerveControllerCommand(
+            traj,
+            this::getPose, // Pose supplier
+            DriveConstants.kDriveKinematics,
+            new PIDController(0, 0, 0),
+            new PIDController(0, 0, 0),
+            new PIDController(0, 0, 0),
+            this::setModuleStates,
+            true,
+            this));
   }
 }
--- 2023_Code/src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ 2023_Code/src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -2,18 +2,30 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
 package frc.robot.subsystems;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.photonvision.*;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
@@ -21,102 +33,141 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 
-
 public class VisionSubsystem extends SubsystemBase {
   /** Creates a new VisionSubsystem. */
   final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(0);
-      final double TARGET_HEIGHT_METERS = Units.feetToMeters(0);
-      final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
+  final double TARGET_HEIGHT_METERS = Units.feetToMeters(0);
+  final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
-  private PhotonCamera camera = new PhotonCamera("gloworm");
+  private PhotonCamera camera;
+  private Transform3d robotToCam;
+  private AprilTagFieldLayout aprilTagFieldLayout;
+  private PhotonPoseEstimator robotPoseEstimator;
   private Buttons buttons;
   private DriveSubsystem m_drive;
 
   public VisionSubsystem(Buttons bs, DriveSubsystem d) {
     buttons = bs;
     m_drive = d;
+
+    camera = new PhotonCamera("gloworm");
+    //Set camera's position relative to the center of the robot
+    robotToCam = new Transform3d(new Translation3d(0.305, 0.0889, 0), new Rotation3d(0, 0, 0));
+
+
+    try {
+      // aprilTagFieldLayout = new AprilTagFieldLayout(AprilTagFields.kDefaultField.m_resourceFile);
+      aprilTagFieldLayout = new AprilTagFieldLayout(Filesystem.getDeployDirectory() + "/2023AprilTagMap.json");
+    } catch (IOException e) {
+      String message = "Unable to open April Tag field layout JSON file. Ensure that it is in the deploy folder. Exception: " + e.toString();
+      System.out.println(message);
+      SmartDashboard.putString("ERROR", message);
+    }
+
+    var camList = new ArrayList<Pair<PhotonCamera, Transform3d>>();
+    camList.add(new Pair<PhotonCamera, Transform3d>(camera, robotToCam));
+    robotPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.AVERAGE_BEST_TARGETS, camera, robotToCam);
   }
-  
-  public double getRange(){
+
+  public double getRange() {
     var result = camera.getLatestResult();
-    double range =
-    PhotonUtils.calculateDistanceToTargetMeters(
-    CAMERA_HEIGHT_METERS,
-    TARGET_HEIGHT_METERS,
-    CAMERA_PITCH_RADIANS,
-    Units.degreesToRadians(result.getBestTarget().getPitch()));
+    double range = PhotonUtils.calculateDistanceToTargetMeters(
+        CAMERA_HEIGHT_METERS,
+        TARGET_HEIGHT_METERS,
+        CAMERA_PITCH_RADIANS,
+        Units.degreesToRadians(result.getBestTarget().getPitch()));
     return range;
   }
-  public boolean hasTargets(){
+
+  public boolean hasTargets() {
     return camera.getLatestResult().hasTargets();
   }
-  public PhotonTrackedTarget getTarget(){
+
+  public PhotonTrackedTarget getTarget() {
     return camera.getLatestResult().getBestTarget();
   }
-  
 
   @Override
   public void periodic() {
-  
+
     // This method will be called once per scheduler run
     SmartDashboard.putNumber("BUTTON PRESSING", -1);
 
     PhotonTrackedTarget target = getTarget();
     int pressed = buttons.getPressedId();
 
-    if (/*target != null &&*/ pressed != 0) {
-    //  double range = getRange();
-     switch(pressed) {
-      case 1:
-        // SmartDashboard.putBoolean("PRESSING ONE WITH TARGET", true);
-        SmartDashboard.putNumber("BUTTON PRESSING", 1);
-        PathPlannerTrajectory traj = PathPlanner.loadPath("New Path", new PathConstraints(2, 2));
-        m_drive.followTrajectoryCommand(traj, true);
-        // System.out.println("PRESSING ONE");
-        break;
-      case 2:
-        // System.out.println("PRESSING TWO");
-        // SmartDashboard.putBoolean("PRESSING TWO WITH TARGET", true);
-        SmartDashboard.putNumber("BUTTON PRESSING", 2);
-        break;
-      case 3:
-        SmartDashboard.putNumber("BUTTON PRESSING", 3);
-        break;
-      case 4:
-        SmartDashboard.putNumber("BUTTON PRESSING", 4);
-        break;
-      case 5:
-        SmartDashboard.putNumber("BUTTON PRESSING", 5);
-        break;
-      case 6:
-        SmartDashboard.putNumber("BUTTON PRESSING", 6);
-        break;
-      case 7:
-        SmartDashboard.putNumber("BUTTON PRESSING", 7);
-        break;
-      case 8:
-        SmartDashboard.putNumber("BUTTON PRESSING", 8);
-        break;
-      case 9:
-        SmartDashboard.putNumber("BUTTON PRESSING", 9);
-        break;
-      case 10:
-        SmartDashboard.putNumber("BUTTON PRESSING", 10);
-        break;
-      // default:
-      //   SmartDashboard.putNumber("BUTTON PRESSING", -1);
-     }
-     
-   }
-   int ID = -1;
-   if (target != null) {
-    ID = target.getFiducialId();
-  }
-  SmartDashboard.putNumber("TARGET ID", (double) ID);
-   
-   
+    if (/* target != null && */ pressed != 0) {
+      // double range = getRange();
+      switch (pressed) {
+        case 1:
+          // SmartDashboard.putBoolean("PRESSING ONE WITH TARGET", true);
+          SmartDashboard.putNumber("BUTTON PRESSING", 1);
+          PathPlannerTrajectory traj = PathPlanner.loadPath("New Path", new PathConstraints(2, 2));
+          m_drive.followTrajectoryCommand(traj, true);
+          // System.out.println("PRESSING ONE");
+          break;
+        case 2:
+          // System.out.println("PRESSING TWO");
+          // SmartDashboard.putBoolean("PRESSING TWO WITH TARGET", true);
+          SmartDashboard.putNumber("BUTTON PRESSING", 2);
+          break;
+        case 3:
+          SmartDashboard.putNumber("BUTTON PRESSING", 3);
+          break;
+        case 4:
+          SmartDashboard.putNumber("BUTTON PRESSING", 4);
+          break;
+        case 5:
+          SmartDashboard.putNumber("BUTTON PRESSING", 5);
+          break;
+        case 6:
+          SmartDashboard.putNumber("BUTTON PRESSING", 6);
+          break;
+        case 7:
+          SmartDashboard.putNumber("BUTTON PRESSING", 7);
+          break;
+        case 8:
+          SmartDashboard.putNumber("BUTTON PRESSING", 8);
+          break;
+        case 9:
+          SmartDashboard.putNumber("BUTTON PRESSING", 9);
+          break;
+        case 10:
+          SmartDashboard.putNumber("BUTTON PRESSING", 10);
+          break;
+        // default:
+        // SmartDashboard.putNumber("BUTTON PRESSING", -1);
+      }
 
-  //  SmartDashboard.putBoolean("Do I Has Targets?", hasTargets());
-  
+    }
+    int ID = -1;
+    if (target != null) {
+      ID = target.getFiducialId();
+    }
+    SmartDashboard.putNumber("TARGET ID", (double) ID);
+    
+
+    // SmartDashboard.putBoolean("Do I Has Targets?", hasTargets());
+
   }
+  
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+    //robotPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+    //robotPoseEstimator.setLastPose(prevEstimatedRobotPose);
+    return robotPoseEstimator.update();
+}
+
+  /*
+  public Pair<Pose3d, Double> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+    robotPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+
+    double currentTime = Timer.getFPGATimestamp();
+    Optional<Pair<Pose3d, Double>> result = robotPoseEstimator.update();
+    if (result.isPresent()) {
+        return new Pair<Pose3d, Double>(result.get().getFirst(), currentTime - result.get().getSecond());
+    } else {
+        return new Pair<Pose3d, Double>(null, 0.0);
+    }
+  }
+    */
 }
